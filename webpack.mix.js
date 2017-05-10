2156f64c4a898135b9dd846a80480d1a9de58234
Paweł <p.schwalkowski@gmail.com>
2017-05-10 22:14:39 +0200

    browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ const { mix } = require('laravel-mix');
  |
  */
 
+mix.browserSync('crm.dev');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
