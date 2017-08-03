77625a3b668bb2a11aa54aacff69ae07bf5e9379
Paweł <pawel.schwalkowski@kbsystems.pl>
2017-08-03 08:19:46 +0200

    Navbar and icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,16 @@
 		<main class="main">Main</main>
 		<aside class="aside">Aside</aside>
 		<nav class="navbar">
-			
+			<div class="profile-wrapper">
+				<!-- <i class="icon ion-search"></i> Search -->
+			</div>
+			<div class="navigation-wrapper">
+				<ul class="main-navigation">
+					<li><a href="#">Projects</a></li>
+					<li><a href="#">Clients</a></li>
+					<li><a href="#">Settings</a></li>
+				</ul>
+			</div>
 		</nav>
 	</div>
 
